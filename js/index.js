2984b9336950355bea3ccad832f926d042798681
Ilya Yalymov <chezeee@gmail.com>
2023-01-29 21:30:14 +0300

    Initial structure added

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import Recipe from "./recipe.js";
+
+// class usage
+const pasta = new Recipe("Pasta", 700);
+console.log(pasta);
+
+const salad = new Recipe("Salad", 350);
+console.log(salad);
---
+++ js/recipe.js
@@ -0,0 +1,9 @@
+// class definition
+export default class Recipe {
+  constructor(name, calories) {
+    this.name = name;
+    this.calories = calories;
+  }
+
+  // your code
+}
